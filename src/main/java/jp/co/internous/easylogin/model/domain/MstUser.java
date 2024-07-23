a417289c99f6a008cb27bd402e9e52d928acf7bb
takutomitaji <t.mitaji13@gmail.com>
2024-07-23 13:37:23 +0900

    SpringBoot

---
+++ src/main/java/jp/co/internous/easylogin/model/domain/MstUser.java
@@ -0,0 +1,40 @@
+package jp.co.internous.easylogin.model.domain;
+
+public class MstUser {
+	
+	private int id;
+	
+	private String userName;
+	
+	private String password;
+	
+	private String fullName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword(String password) {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void getFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+}
+
+
